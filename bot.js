1957b985246195c6b3eba905d289ccf09c473f88
shawn5961 <shawn.meas89@gmail.com>
2020-03-16 18:55:18 -0400

    Testing youtube-dl

--- bot.js
+++ bot.js
@@ -33,17 +33,9 @@ client.on('message', function(message) {
     function consoleLog(word, index){
         console.log(word);
         if (youtubeRegex.test(word)){
-            //youtubedl.exec(word, ['-x', '--audio-format', 'mp3', '-o', '\"%(title)s.%(ext)s\"']);
+            youtubedl.exec(word, ['-x', '--audio-format', 'mp3', '-o', '\"%(title)s.%(ext)s\"']);
             //const video = youtubedl(word, ['-x --audio-format mp3 -o \"%(title)s.%(ext)s\"']);
             message.channel.send('Word # ' + index + 'is a youtube link');
-
-            video.on('info', function(info) {
-                console.log('Download started')
-                console.log('filename: ' + info._filename)
-                console.log('size: ' + info.size)
-            })
-
-            video.pipe(fs.createWriteStream('myvideo.mp3'))
         }
     }
 
