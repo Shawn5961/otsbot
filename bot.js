a91ce1dc681368d400ec3577e7557c5d9cca512b
shawn5961 <shawn.meas89@gmail.com>
2020-03-16 15:48:16 -0400

    Updated to new API

--- bot.js
+++ bot.js
@@ -9,15 +9,10 @@ logger.add(new logger.transports.Console, {
 logger.level = 'debug';
 
 // Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
+const client = new Discord.Client();
 
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+client.once('ready', () => {
+    console.log('Ready!');
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
