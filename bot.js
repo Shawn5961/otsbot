24e136b96aa872868544e070adc51759212fb5a8
shawn5961 <shawn.meas89@gmail.com>
2020-03-16 15:35:48 -0400

    Changed command to !ots

--- bot.js
+++ bot.js
@@ -7,16 +7,19 @@ logger.add(new logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
+
 // Initialize Discord Bot
 var bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
+
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
+
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
@@ -26,8 +29,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
        
         args = args.splice(1);
         switch(cmd) {
-            // !ping
-            case 'ping':
+            // Check message for YouTube links
+            case 'ots':
                 bot.sendMessage({
                     to: channelID,
                     message: 'Pong!'
